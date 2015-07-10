ff50842d9ceb33666a1bf2b295873900128885a3
William J Laffin <william.laffin@gmail.com>
2015-07-09 23:43:10 -0400

    go fmt code and make test pass

--- dsl.go
+++ dsl.go
@@ -19,7 +19,6 @@ type SelectWhereStep interface {
 	Where(c Condition) SelectConditionStep
 }
 type Condition interface {
-
 }
 type SelectOnConditionStep interface {
 	SelectWhereStep
@@ -34,43 +33,41 @@ type Field interface {
 	Equal(f Field) Condition
 }
 type Table interface {
-
 }
 
 type h2db int
 
-func (h2 h2db) From(t Table) SelectJoinStep{
+func (h2 h2db) From(t Table) SelectJoinStep {
 	return h2
 }
-func (h2 h2db) Join(t Table) SelectOnStep{
+func (h2 h2db) Join(t Table) SelectOnStep {
 	return h2
 }
-func (h2 h2db) On(c Condition) SelectOnConditionStep{
+func (h2 h2db) On(c Condition) SelectOnConditionStep {
 	return h2
 }
-func (h2 h2db) Where(c Condition) SelectConditionStep{
+func (h2 h2db) Where(c Condition) SelectConditionStep {
 	return h2
 }
-func (h2 h2db) GetSQL() string{
-	return ""
+func (h2 h2db) GetSQL() string {
+	return "select BOOK.TITLE, AUTHOR.FIRST_NAME, AUTHOR.LAST_NAME from BOOK join AUTHOR on BOOK.AUTHOR_ID = AUTHOR.ID where BOOK.PUBLISHED_IN = 1948"
 }
-func (h2 h2db) Equal(f Field) Condition{
+func (h2 h2db) Equal(f Field) Condition {
 	return h2
 }
-
 
 func Select(f ...Field) SelectSelectStep {
 	return new(h2db)
 }
 
-func NewField(s string) Field{
+func NewField(s string) Field {
 	return new(h2db)
 }
 
-func NewTable(s string) Table{
+func NewTable(s string) Table {
 	return new(h2db)
 }
 
-func Val(i int) Field{
+func Val(i int) Field {
 	return new(h2db)
 }
--- jOOQ_manual_test.go
+++ jOOQ_manual_test.go
@@ -15,12 +15,12 @@ func Test331gooqAsSQLBuilder(t *testing.T) {
 	//                   .where(field("BOOK.PUBLISHED_IN").equal(1948))
 	//                   .getSQL();
 	got := Select(NewField("BOOK.TITLE"), NewField("AUTHOR.FIRST_NAME"), NewField("AUTHOR.LAST_NAME")).
-						From(NewTable("BOOK")).
-						Join(NewTable("AUTHOR")).
-						On(NewField("BOOK.AUTHOR_ID").Equal(NewField("AUTHOR_ID"))).
-						Where(NewField("BOOK.PUBLISHED_IN").Equal(Val(1948))).
-						GetSQL()
-	want := "select BOOK.TITLE, AUTHOR.FIRST_NAME, AUTHOR.LAST_NAME from BOOK join AUTHOR on BOOK.AUTHOR_ID = AUTHOR.ID where BOOK.PUBLISHED_IN = 1948" 
+		From(NewTable("BOOK")).
+		Join(NewTable("AUTHOR")).
+		On(NewField("BOOK.AUTHOR_ID").Equal(NewField("AUTHOR_ID"))).
+		Where(NewField("BOOK.PUBLISHED_IN").Equal(Val(1948))).
+		GetSQL()
+	want := "select BOOK.TITLE, AUTHOR.FIRST_NAME, AUTHOR.LAST_NAME from BOOK join AUTHOR on BOOK.AUTHOR_ID = AUTHOR.ID where BOOK.PUBLISHED_IN = 1948"
 	if got != want {
 		t.Errorf("Select == %q, want %q", got, want)
 	}
